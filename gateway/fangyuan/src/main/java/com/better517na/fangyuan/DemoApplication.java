31c45cbb8d2ca07d95ad486e2eefab0871eb03f1
vril <vrilcc@163.com>
2019-08-20 17:00:08 +0800

    add gateway

---
+++ gateway/fangyuan/src/main/java/com/better517na/fangyuan/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.better517na.fangyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+       return builder.routes()
+        .route(p -> p
+            .path("/get")
+            // .filters(f -> f.addRequestHeader("Hello", "World"))
+            .uri("http://httpbin.org:80"))
+        .build();
+    }
+}
