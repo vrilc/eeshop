18a41d3fd777740946bcd117d3354e4a4d39a052
vril <vrilcc@163.com>
2019-08-20 11:41:04 +0800

    add get cloud config test service

---
+++ config-get/fangyuan/src/main/java/com/better517na/fangyuan/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.better517na.fangyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
