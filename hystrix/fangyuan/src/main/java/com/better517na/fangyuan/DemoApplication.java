026441458e058f84299061af3212c4020a79677a
vril <vrilcc@163.com>
2019-08-26 11:36:58 +0800

    one week work

--- hystrix/fangyuan/src/main/java/com/better517na/fangyuan/DemoApplication.java
+++ hystrix/fangyuan/src/main/java/com/better517na/fangyuan/DemoApplication.java
@@ -1,20 +1,43 @@
 package com.better517na.fangyuan;
 
+<<<<<<< HEAD
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
+=======
+>>>>>>> parent of 851fe8a... add hystrix for feign
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableHystrix
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrixDashboard
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+<<<<<<< HEAD
+	
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+=======
 
+>>>>>>> parent of 851fe8a... add hystrix for feign
 }
